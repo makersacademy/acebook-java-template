394dbc97fb9ee94bf31c4ca2555e78c6579d71ea
Edward Andress <eddie_andress@hotmail.com>
2021-10-29 14:49:37 +0100

    add getters and setters to User and Post

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -23,5 +23,7 @@ public class Post {
     public Post(String content) {
         this.content = content;
     }
+    public String getContent() { return this.content; }
+    public void setContent(String content) { this.content = content; }
 
 }
