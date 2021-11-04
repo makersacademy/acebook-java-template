b8160243a48e7d8fb32391551eb225fcf579b779
Joe <josephshaw1563@gmail.com>
2021-11-04 11:26:49 +0000

    before breaking code

--- src/main/resources/db/migration/V5__create_like_table.sql
+++ src/main/resources/db/migration/V5__create_like_table.sql
@@ -1,6 +1,8 @@
 
 CREATE TABLE liked_posts(
+
 user_id bigint NOT NULL references users (id), 
-post_id bigint NOT NULL references posts (id)
+post_id bigint NOT NULL references posts (id),
+PRIMARY KEY(user_id, post_id)
 
 );
