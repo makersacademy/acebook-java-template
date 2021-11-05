6fb696873d776f960874a5ed8db6d96d9eee941a
Delia <delia.bute@yahoo.com>
2021-11-05 12:46:52 +0000

    add delete tests

--- src/test/java/PostsTest.java
+++ src/test/java/PostsTest.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import com.github.javafaker.Faker;
 import com.makersacademy.acebook.Application;
 import org.junit.After;
@@ -6,10 +8,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import org.openqa.selenium.support.pagefactory.ByAll;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -79,12 +85,43 @@ public class PostsTest {
     // making second post
     driver.findElement(By.id("content")).sendKeys("Hello World!");
     driver.findElement(By.id("submit")).click();
+
     String bodyText1 = driver.findElement(By.tagName("body")).getText();
     Assert.assertTrue("checks that the initial post appears", bodyText1.contains("The weather today is sunny!"));
     Assert.assertTrue("checks that the second post appears", bodyText1.contains("Hello World!"));
   }
+
   // Test dateAppears
   // Test nameAppears
   // Test pictureAppears
   // Test postsShownInReverseChronologicalOrder
+
+  @Test
+  public void deletePost() {
+    // signup
+    driver.get("http://localhost:8080/users/new");
+    String name = faker.name().firstName();
+    driver.findElement(By.id("username")).sendKeys(name);
+    driver.findElement(By.id("password")).sendKeys("password");
+    driver.findElement(By.id("submit")).click();
+
+    // sign in
+    driver.findElement(By.id("username")).sendKeys(name);
+    driver.findElement(By.id("password")).sendKeys("password");
+    driver.findElement(By.xpath("//button")).click();
+
+    // making posts
+    driver.findElement(By.id("content")).sendKeys("Happy days");
+    driver.findElement(By.id("submit")).click();
+    driver.findElement(By.id("content")).sendKeys("Happy morning");
+    driver.findElement(By.id("submit")).click();
+
+    // delete post
+    List<WebElement> a = driver.findElements(By.id("delete"));
+    a.get(0).click();
+    driver.switchTo().alert().accept(); // handle popup window
+
+    String bodyText = driver.findElement(By.tagName("body")).getText();
+    Assert.assertFalse("checks that the post appears", bodyText.contains("Happy days"));
+  }
 }
