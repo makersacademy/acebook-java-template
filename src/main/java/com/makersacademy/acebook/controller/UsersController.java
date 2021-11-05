f6ceabd790ef3ae339ba4adf643489807b29d507
Sunny Donaldson <sunnydonaldson1@gmail.com>
2021-11-05 16:30:17 +0000

    rendering taken message on signup view

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.makersacademy.acebook.service.IUserService;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UsersController {
 
@@ -25,20 +26,32 @@ public class UsersController {
     @Autowired
     AuthoritiesRepository authoritiesRepository;
 
-    @GetMapping("/users/new")
-    public String signup(Model model) {
+    @GetMapping({"/users/new{userExists}"})
+    public String signup(Model model, @RequestParam(name="userExists", required = false, defaultValue = "false") Boolean userExists) {
+        model.addAttribute("userExists", new Boolean(userExists));
         model.addAttribute("user", new User());
         return "users/new";
     }
+    // public String signup(Model model, @ModelAttribute boolean userExists){
+    //     // model.addAttribute("userExists", userExists);
+    //     model.addAttribute("user", new User());
+    //     return "users/new";
+    // }
 
     @PostMapping("/users")
     public RedirectView signup(@ModelAttribute User user) {
         //userRepository.save(user);
         if(userService.save(user)){
+            System.out.println("user saved");
             Authority authority = new Authority(user.getUsername(), "ROLE_USER");
             authoritiesRepository.save(authority);
+            
+            return new RedirectView("/login.html");
         }
-        return new RedirectView("/login.html");
+        System.out.println("user not saved");
+        // redirectAttributes.addFlashAttribute("userExists", true);
+        return new RedirectView("/users/new?userExists=true");
+        
 
 
     }
