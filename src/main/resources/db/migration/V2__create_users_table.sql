05f119a06a1c74527cf6c3e228382826ffda0e98
ewa <ewa.jablonskaa@gmail.com>
2021-11-09 08:55:06 +0000

    users table update

--- src/main/resources/db/migration/V2__create_users_table.sql
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id bigserial PRIMARY KEY,
   username varchar(50) NOT NULL UNIQUE,
-  password varchar(60) NOT NULL,
+  password varchar(50) NOT NULL,
   enabled boolean NOT NULL
 );
 
