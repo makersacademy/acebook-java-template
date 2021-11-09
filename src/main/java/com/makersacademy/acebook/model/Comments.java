898198494a9084478a9b442bfc39c1b6423e36f1
Delia <delia.bute@yahoo.com>
2021-11-09 16:39:25 +0000

    initialized comments class

---
+++ src/main/java/com/makersacademy/acebook/model/Comments.java
@@ -0,0 +1,55 @@
+package com.makersacademy.acebook.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.GenerationType;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "COMMENTS")
+public class Comments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    public String content;
+    @CreationTimestamp
+    public Timestamp time;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    public User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    public Post post;
+
+    public Comments() {
+    }
+
+    public Comments(String content, Timestamp time, Long id) {
+        this.content = content;
+        this.time = time;
+        this.id = id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    // public void setUser(User user) {this.user = user;}
+
+}
