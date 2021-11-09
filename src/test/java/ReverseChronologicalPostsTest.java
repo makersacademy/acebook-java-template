c7f9d2f9b39691f49565b54b39a657983cdb8234
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-11-09 10:56:19 +0000

    commiting as main merge hasn't properly worked and post files appear as they did pre-service-removal

--- src/test/java/ReverseChronologicalPostsTest.java
+++ src/test/java/ReverseChronologicalPostsTest.java
@@ -1,5 +1,6 @@
 import com.github.javafaker.Faker;
 import com.makersacademy.acebook.Application;
+import com.makersacademy.acebook.model.Post;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -35,7 +36,26 @@ public class ReverseChronologicalPostsTest {
     }
 
     @Test
-    public void testPostsDisplayInReverseChronologicalOrder() {
-
+    public void successfulPostsDisplayInReverseChronologicalOrder() {
+        driver.get("http://localhost:8080/users/new");
+        driver.findElement(By.id("username")).sendKeys(faker.name().firstName());
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.id("submit")).click();
+        driver.get("http://localhost:8080/posts");
+        driver.findElement(By.id("content-input")).sendKeys("1");
+        driver.findElement(By.id("submit")).click();
+        driver.findElement(By.id("content-input")).sendKeys("2");
+        driver.findElement(By.id("submit")).click();
+        driver.findElement(By.id("content-input")).sendKeys("3");
+        driver.findElement(By.id("submit")).click();
+        driver.findElement(By.id("content-input")).sendKeys("4");
+        driver.findElement(By.id("submit")).click();
+    //     List<WebElement> photoElements = driver.findElements(By.className("photo"));
+    // for (WebElement photoEl : photoElements) {
+    //   String src = photoEl.getAttribute("src");
+    //   if (src == photoUrl) {
+    //     Assert.assertEquals(photoUrl, src);
+    //   }
     }
+
 }
