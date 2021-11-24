3668f975fe60d263cbe455e0ee12c12e245116d0
Jenaro Garcia <jjegarcia@hotmail.com>
2021-11-24 18:18:26 +0000

    tidyup

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
-
 import java.time.LocalDateTime;
 
 @Controller
