a7371599e1c25a0c24c3e784c08528e4c5ae1993
Jamie Bowers <jamiebowers3@gmail.com>
2021-11-25 16:25:16 +0000

    work in progress

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Authority;
+import com.makersacademy.acebook.model.ErrorMessages;
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.AuthoritiesRepository;
 import com.makersacademy.acebook.repository.UserRepository;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.sql.ResultSet;
 
 @Controller
 public class UsersController {
@@ -31,14 +34,19 @@ public class UsersController {
 
     @PostMapping("/users")
     public RedirectView signup(@ModelAttribute User user) {
-        if (user.getPassword().length()>0) {
-            user.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(user);
-            Authority authority = new Authority(user.getUsername(), "ROLE_USER");
-            authoritiesRepository.save(authority);
-            return new RedirectView("/login");
-        }
-        else{
+        try {
+            if (user.getPassword().length() > 0) {
+                user.setPassword(encoder.encode(user.getPassword()));
+                userRepository.save(user);
+                Authority authority = new Authority(user.getUsername(), "ROLE_USER");
+                authoritiesRepository.save(authority);
+                return new RedirectView("/login");
+            } else {
+                return new RedirectView("/users/new");
+            }
+        } catch (Exception e) {
+            ErrorMessages error = new ErrorMessages();
+            error.setErrorMessage();
             return new RedirectView("/users/new");
         }
     }
---
+++ src/main/java/com/makersacademy/acebook/model/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.makersacademy.acebook.model;
+
+public class ErrorMessages {
+    String errorMessage;
+
+    public void setErrorMessage(){
+        errorMessage = "Invalid Login";
+    }
+}
