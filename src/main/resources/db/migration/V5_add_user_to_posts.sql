c7a3e51f7992342505b025fcae201c76bae733f1
Jenaro Garcia <jjegarcia@hotmail.com>
2021-11-25 14:09:31 +0000

    WIP tables not runing

---
+++ src/main/resources/db/migration/V5_add_user_to_posts.sql
@@ -0,0 +1,13 @@
+ALTER TABLE posts ADD username varchar(50) NOT NULL;
+ALTER TABLE posts ADD likes int NOT NULL;
+ALTER TABLE posts ADD   constraint fk_posts_users foreign key(username) references users(username);
+
+CREATE TABLE profile (
+  id bigserial PRIMARY KEY,
+  username varchar(50) NOT NULL,
+  profile_picture varchar(300) NOT NULL,
+  constraint fk_profile_users foreign key(username) references users(username)
+);
+
+
+
