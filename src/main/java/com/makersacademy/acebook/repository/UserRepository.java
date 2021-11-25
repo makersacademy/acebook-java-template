b9d488bcf53f32a21f0c883429e74a68da29fe3a
Jack Storror <90349541+JackStorror@users.noreply.github.com>
2021-11-25 11:19:56 +0000

    Update UserRepository.java

--- src/main/java/com/makersacademy/acebook/repository/UserRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
