94579f4f8da1f996bebd250f923ff3d17855ccd9
Jack Storror <jackstorror@Jacks-MacBook-Pro.local>
2021-11-25 13:08:34 +0000

    for better security, added post logout button

--- src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
@@ -37,7 +37,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
        .and()
                 .logout()
                     .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
                     .clearAuthentication(true)
                     .invalidateHttpSession(true)
                     .deleteCookies("JSESSIONID", "rememeber-me")
