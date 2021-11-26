9cfba18ca57a4fb0d56737c78b74028b2345adb0
Jamie Bowers <jamiebowers3@gmail.com>
2021-11-26 15:19:52 +0000

    Refactor and added test

--- src/test/java/com/makersacademy/acebook/model/PostListTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostListTest.java
@@ -14,8 +14,8 @@ import static org.junit.Assert.*;
 
 public class PostListTest {
 //    private LocalDateTime testDate = LocalDateTime.now();
-	private Post post = new Post("hello");
-	private Post post2 = new Post("hello");
+	private Post post = new Post("Old Entry");
+	private Post post2 = new Post("Latest Entry");
 	private ArrayList<Post> iterables = new ArrayList<Post>(Arrays.asList(post, post2));
 	private PostList postList = new PostList();
 
@@ -24,6 +24,8 @@ public class PostListTest {
 	@Test
 	public void postListReturnsArrayOfPost() {postList.setList(iterables); assertThat(postList.getList().get(0), instanceOf(Post.class)); }
 	@Test
-	public void postListReturnsSpecificPost() {postList.setList(iterables); assertEquals("List item is post", postList.getList().get(0), post); }
+	public void postListReturnsSpecificPost() {postList.setList(iterables); assertEquals("List item is post", postList.getList().get(1), post); }
+	@Test
+	public void postListSortList() {postList.setList(iterables); assertEquals(postList.postArrayList.get(0).getContent(), "Latest Entry");}
 }
 
