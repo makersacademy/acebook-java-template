fcfcdc506d5a17f838a8186b96977a214962a6c2
Jamie Bowers <jamiebowers3@gmail.com>
2021-11-29 11:37:41 +0000

    Post tests added

--- src/test/java/com/makersacademy/acebook/model/PostTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostTest.java
@@ -6,10 +6,13 @@ import java.time.LocalDateTime;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class PostTest {
     private LocalDateTime testDate = LocalDateTime.now();
+	private String content = "content";
+	private String username = "username";
 	private Post post = new Post("hello");
 
 	@Test
@@ -20,4 +23,18 @@ public class PostTest {
 	public void postHasDate() { post.setTime(LocalDateTime.now()); assertThat(post.getDate(), instanceOf(LocalDateTime.class)); }
 	@Test
 	public void postHasFormattedDate() { post.setTime(LocalDateTime.now()); assertThat(post.getFormattedDate(), instanceOf(String.class)); }
+	@Test
+	public void getContentTest(){
+		post.setContent(content);
+		assertEquals(post.getContent(), content);
+	}
+	@Test
+	public void populateTest(){
+		post.populate(content, testDate, username, 0);
+		assertEquals(post.getContent(), content);
+		assertEquals(post.getDate(), testDate);
+		assertEquals(post.getUsername(), username);
+		assertEquals(post.getLikes(), 0);
+	}
 }
+
