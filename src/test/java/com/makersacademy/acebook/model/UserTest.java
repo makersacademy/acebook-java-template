bb3d574f7d35b6d726c4bf67d80e5eaf3195c44f
Jamie Bowers <jamiebowers3@gmail.com>
2021-11-29 10:44:14 +0000

    Refactor and setUsername test

--- src/test/java/com/makersacademy/acebook/model/UserTest.java
+++ src/test/java/com/makersacademy/acebook/model/UserTest.java
@@ -3,22 +3,27 @@ package com.makersacademy.acebook.model;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class UserTest {
+    String username = "TestUsername";
+    String password = "TestPassword";
+    User user = new User(username, password);
 
     @Test
     public void getUsernameTest(){
-        String username = "TestUsername";
-        String password = "TestPassword";
-        User user = new User(username, password);
         assertEquals( user.getUsername(), username );
     }
 
     @Test
     public void getPasswordTest(){
-        String username = "TestUsername";
-        String password = "TestPassword";
-        User user = new User(username, password);
         assertEquals( user.getPassword(), password );
+    }
+
+    @Test
+    public void setUsername(){
+        String setUser = "setUsername";
+        user.setUsername(setUser);
+        assertEquals(user.getUsername(), setUser);
     }
 }
