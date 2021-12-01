9adbca8747413d33dbb32bc5e20700a3a6e38985
Jenaro Garcia <jjegarcia@hotmail.com>
2021-12-01 11:16:13 +0000

    added post_id and its reference

--- src/main/resources/db/migration/V6__add_likes_table.sql
+++ src/main/resources/db/migration/V6__add_likes_table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE likes (
   id bigserial PRIMARY KEY,
+  post_id bigint,
   username varchar(50) NOT NULL,
-  constraint fk_likes_users foreign key(username) references users(username)
+  constraint fk_likes_users foreign key(username) references users(username),
+  constraint fk_likes_posts foreign key(post_id) references posts(id)
 );
