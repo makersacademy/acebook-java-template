d0b9f21ac6e31307665970ea8df5ecf346722d64
Jenaro Garcia <jjegarcia@hotmail.com>
2021-12-02 20:30:00 +0000

    added post_id set and setlike in like class

--- src/main/java/com/makersacademy/acebook/model/Like.java
+++ src/main/java/com/makersacademy/acebook/model/Like.java
@@ -5,13 +5,23 @@ import lombok.Data;
 import javax.persistence.*;
 
 @Data
-    @Entity
-    @Table(name = "POSTS")
-    public class Like {
+@Entity
+@Table(name = "POSTS")
+public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    private Long post_id;
+
+
+    public Long getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(Long post_id) {
+        this.post_id = post_id;
+    }
 
     public Long getId() {
         return id;
@@ -29,4 +39,8 @@ import javax.persistence.*;
         this.username = username;
     }
 
- }
+    public void setLike(String username, Long post_id) {
+        setUsername(username);
+        setPost_id(post_id);
+    }
+}
