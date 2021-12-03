f642a130d2442ac42f7052e0655f0d251f0be2a9
Jenaro Garcia <jjegarcia@hotmail.com>
2021-12-03 10:26:53 +0000

    tidy up

--- src/main/java/com/makersacademy/acebook/model/LikesHandler.java
+++ src/main/java/com/makersacademy/acebook/model/LikesHandler.java
@@ -1,11 +1,9 @@
 package com.makersacademy.acebook.model;
 
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Stream;
 
 public class LikesHandler {
     private String username;
@@ -30,30 +28,13 @@ public class LikesHandler {
     }
 
     private Boolean postHasUser(Post post, Iterable<Like> iLikeList) {
-
         LikesList olikeList = new LikesList();
         olikeList.setList(iLikeList);
-        System.out.println("olike----> ");
-        System.out.println(olikeList);
-
         ArrayList<Like> arrayList = olikeList.likeArrayList;
         List<Like> test1 = arrayList.stream().filter(like ->
                 like.getPost_id().equals(post.getId())
         ).toList();
-        System.out.println("test1----> ");
-//        printerListLike(test1);
-        System.out.println(test1);
-
-
         List<String> test2 = test1.stream().map(Like::getUsername).toList();
-
-        System.out.println("test2----> ");
-//        printerList(test2);
-        System.out.println(test2);
-
-
-        System.out.println("loggedin----> ");
-        System.out.println(username);
 
         AtomicBoolean match = new AtomicBoolean(false);
         test2.forEach(user -> {
@@ -62,56 +43,7 @@ public class LikesHandler {
                     }
                 }
         );
-
-        System.out.println("match----> ");
-        System.out.println(match);
-
-
         return match.get();
-
-    }
-
-    private void printerListLike(List<Like> likeList) {
-        likeList.forEach(like -> {
-            System.out.println("list<Like>--->");
-            System.out.println(like);
-        });
-    }
-
-    private void printer(LikesList likeList) {
-        likeList.likeArrayList.forEach(like -> {
-            System.out.println("like--->");
-            System.out.println(like);
-        });
-    }
-
-    private void printerI(Iterable<Like> likeList) {
-        likeList.forEach(like ->
-        {
-            System.out.println("Iterable<Like>--->");
-            System.out.println(like);
-        });
-    }
-
-    private void printerL(Stream<Like> likeList) {
-        likeList.forEach(like -> {
-            System.out.println("Stream<Like>--->");
-            System.out.println(like);
-        });
-    }
-
-    private void printerS(Stream<String> likeList) {
-        likeList.forEach(like -> {
-            System.out.println("Stream<String>--->");
-            System.out.println(like);
-        });
-    }
-
-    private void printerList(List<String> likeList) {
-        likeList.forEach(like -> {
-            System.out.println("List<String>--->");
-            System.out.println(like);
-        });
     }
 
 }
