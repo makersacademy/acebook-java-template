9a0f8fa488daebd62a8594fe5a43570b6af542ee
Viktor Vrecko <vvrecko@posteo.de>
2022-02-15 17:50:46 +0100

    Post.java getter & setter

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -30,5 +30,6 @@ public class Post {
      public void setContent(String content) { this.content = content; }
      public Long getId() { return this.id; }
      public void setId(Long id) { this.id = id; }
-
+     public String getAuthor() { return this.author; }
+     public void setAuthor(String author) { this.author = author; }
 }
