45a23d8f2f4867ffd6eeb280283f92bd78458060
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-04-20 14:17:47 +0100

    test for timestamp method

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -1,5 +1,8 @@
 package com.makersacademy.acebook.model;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,13 +20,30 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    // private String userWhoPosted;
+    private Timestamp timestamp;
 
     public Post() {}
 
     public Post(String content) {
         this.content = content;
+        this.timestamp = getTime();
     }
+
+
+    public Timestamp getTime() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat formatTimestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        formatTimestamp.format(timestamp);
+        return timestamp;
+    }
+
     public String getContent() { return this.content; }
     public void setContent(String content) { this.content = content; }
+
+    public Timestamp getTimeStamp() { return this.timestamp; }
+
+    // public String getUser() { return this.userWhoPosted; }
+    // public void setUser(String userWhoPosted) { this.userWhoPosted = userWhoPosted; }
 
 }
