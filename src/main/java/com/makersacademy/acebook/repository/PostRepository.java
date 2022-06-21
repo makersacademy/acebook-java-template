d58254ee0965da1d523b9e3d64857d953eed77de
Shahin <shahin.sadeghi13@gmail.com>
2022-06-21 17:04:29 +0200

    Search feature

--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Post;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-
+    List<Post> findByContentContaining(String infix);
 }
