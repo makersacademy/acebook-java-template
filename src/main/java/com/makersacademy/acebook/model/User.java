3e5d13bad1310982e8dcd700a9371dd9aa0059a7
VictorMiqueles <victormiqueles@hotmail.com>
2022-06-22 15:48:39 +0100

    Begin adding email to sign up

--- src/main/java/com/makersacademy/acebook/model/User.java
+++ src/main/java/com/makersacademy/acebook/model/User.java
@@ -18,6 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    //private String email;
     private String password;
     private boolean enabled;
 
