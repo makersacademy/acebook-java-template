0cbcae33537bdd6ae411ed2da1fbfa7f0deb0096
Theo <theo@TNT-DESKTOP.localdomain>
2022-06-23 14:58:59 +0100

    theo1 branch

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
