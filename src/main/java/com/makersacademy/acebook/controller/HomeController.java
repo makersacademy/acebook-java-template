3c5b6bb0b3167348a4f18e473a676b3329fc0ff8
Endre Barabas <barabasandrew@gmail.com>
2022-06-24 12:53:00 +0100

    rebasing

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.makersacademy.acebook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
