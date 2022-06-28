f2c90cb17ee5c1cda09723bc9b05902b425eb259
realtnt <theo@thetaphoto.co.uk>
2022-06-29 00:07:36 +0100

    Feed now filtered to show one own and friend posts

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -2,8 +2,10 @@ package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.model.Friend;
 import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
+import com.makersacademy.acebook.repository.FriendRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,11 +24,29 @@ public class PostsController {
   PostRepository postRepository;
   @Autowired
   UserRepository userRepository;
+  @Autowired
+  FriendRepository friendRepository;
 
   @GetMapping("/posts")
-  public String index(Model model, @ModelAttribute Post post) {
-    List<Post> posts = postRepository.findAllByOrderByTimeDesc();
-    model.addAttribute("posts", posts);
+  public String index(Model model, Principal principal, @ModelAttribute Post formPost) {
+    User user = userRepository.findByUsername(principal.getName()).get(0);
+    List<Post> allPosts = postRepository.findAllByOrderByTimeDesc();
+    List<Friend> friends = friendRepository.findByUserIdAndAccepted(user.getId(), true);
+    List<Long> friendsIds = new ArrayList<Long>();
+    List<Post> friendPosts = new ArrayList<Post>();
+
+    friendsIds.add(user.getId());
+    for (Friend friend : friends) {
+      friendsIds.add(friend.getFriend().getId());
+    }
+
+    for (Post post : allPosts) {
+      if (friendsIds.contains(post.getUser().getId())) {
+        friendPosts.add(post);
+      }
+    }
+
+    model.addAttribute("posts", friendPosts);
     return "posts/feed";
   }
 
