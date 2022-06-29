ebd812aea190c6b18d909a890656e6ab6a176b39
realtnt <theo@thetaphoto.co.uk>
2022-06-29 08:07:59 +0100

    Removed unnecessary imports

--- src/main/java/com/makersacademy/acebook/controller/LikesController.java
+++ src/main/java/com/makersacademy/acebook/controller/LikesController.java
@@ -9,7 +9,6 @@ import com.makersacademy.acebook.repository.PostRepository;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
