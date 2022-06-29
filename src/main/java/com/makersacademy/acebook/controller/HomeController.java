251f6125d01544ac3f7198915e68f6fd6e8e532c
realtnt <theo@thetaphoto.co.uk>
2022-06-29 08:06:52 +0100

    Removed unnecessary imports

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,14 +1,13 @@
 package com.makersacademy.acebook.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/")
-	public RedirectView index() {
-		return new RedirectView("/posts");
-	}
+  @RequestMapping(value = "/")
+  public RedirectView index() {
+    return new RedirectView("/posts");
+  }
 }
--- src/main/java/com/makersacademy/acebook/model/Friend.java
+++ src/main/java/com/makersacademy/acebook/model/Friend.java
@@ -1,15 +1,12 @@
 package com.makersacademy.acebook.model;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import javax.persistence.GenerationType;
