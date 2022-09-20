bfe25f062209485b62c2dcdba2fec7abd0932e47
BorisYTTsang <boris.yttsang@outlook.com>
2022-09-20 18:01:26 +0100

    -m logout conflicts resolved

---
+++ src/test/java/LogOutTest.java
@@ -0,0 +1,80 @@
+import com.github.javafaker.Faker;
+import com.makersacademy.acebook.Application;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+
+public class LogOutTest {
+  
+  WebDriver driver;
+  Faker faker;
+
+  @Before
+  public void setup() {
+      System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+      driver = new ChromeDriver();
+      faker = new Faker();
+  }
+
+  @After
+  public void tearDown() {
+      driver.close();
+  }
+
+  @Test
+  public void logOutReturnsLogOutPageForUser() {
+    // There's no post method for WebDriver unfortunately, only get
+      driver.get("http://localhost:8080/users/new");
+      driver.findElement(By.id("username")).sendKeys("ash");
+      driver.findElement(By.id("password")).sendKeys("password");
+      driver.findElement(By.id("submit")).click();
+      driver.get("http://localhost:8080/logout");
+      // This line is test clicking logout button on index.html but doesn't work yet (will replace above line)
+      // driver.findElement(By.id("Logout")).click();
+      Assert.assertEquals(driver.getTitle(), "Confirm Log Out?");
+  }
+
+  @Test
+  public void clickinglogOutOnLogOutPageLogsUserOut() {
+    driver.get("http://localhost:8080/users/new");
+    driver.findElement(By.id("submit")).click();
+    driver.get("http://localhost:8080/logout");
+    // findElement(By.id) didn't work for logout page, so used By.tagName for Log Out <button>
+    driver.findElement(By.tagName("button")).click();
+    // Tests title of page to equal log in page's
+    Assert.assertEquals(driver.getTitle(), "Please sign in");
+  }
+
+  // Test to see if session exists
+
+  // @Test
+  // public void logInCreatesCookies() {
+  //   driver.get("http://localhost:8080/login");
+  //   driver.findElement(By.id("username")).sendKeys("boris");
+  //   driver.findElement(By.id("password")).sendKeys("password123");
+  //   driver.findElement(By.id("submit")).click();
+  //   Assert.assertTrue(driver.manage().getCookieNamed("boris");
+  // }
+
+  // Test for redirecting to login page from /logout if not logged in
+
+  // @Test
+  // public void logOutRedirectsGuestToLogInPage() {
+  //     driver.get("http://localhost:8080/logout");
+  //     String title = driver.getTitle();
+  //     Assert.assertEquals("Please sign in", title);
+  // }
+
+}
