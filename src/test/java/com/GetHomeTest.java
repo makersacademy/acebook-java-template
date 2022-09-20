f6aeeeab2c08c60ecbab6a0729131d809d106fe8
Connor <swagdecon77@gmail.com>
2022-09-20 17:37:41 +0100

    1 test added and one in progress

---
+++ src/test/java/com/GetHomeTest.java
@@ -0,0 +1,59 @@
+package com;
+
+import org.junit.Test;
+import com.github.javafaker.Faker;
+import com.makersacademy.acebook.Application;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By.ByClassName;
+import org.openqa.selenium.By.ByLinkText;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+
+public class GetHomeTest {
+    WebDriver driver;
+    Faker faker;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        driver = new ChromeDriver();
+        faker = new Faker();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void getslashreturnsyoutosigninifnotloggedin() {
+        driver.get("http://localhost:8080/");
+        String title = driver.getTitle();
+        Assert.assertEquals("Please sign in", title);
+    }
+
+    @Test
+    public void getslashreturnsyoutopostsifsignedin() {
+        driver.get("http://localhost:8080/users/new");
+        driver.findElement(By.id("username")).sendKeys(faker.name().firstName());
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.id("submit")).click();
+        String title = driver.getTitle();
+        Assert.assertEquals("Please sign in", title);
+        driver.get("http://localhost:8080/login");
+        driver.findElement(By.id("username")).sendKeys(faker.name().firstName());
+        driver.findElement(By.id("password")).sendKeys("password");
+        // driver.findElement(By.
+        // Assert.assertEquals("Posts", title);
+    }
+}
