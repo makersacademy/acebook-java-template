8ba08f8e530d401558ba83668325b947c487762c
hakareem <harithakareem72@gmail.com>
2022-09-21 15:14:15 +0100

    updated the user table && updated form pattern

--- src/main/resources/db/migration/V2__create_users_table.sql
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -11,7 +11,7 @@ CREATE TABLE authorities (
   id bigserial PRIMARY KEY,
   username varchar(50) NOT NULL,
   authority varchar(50) NOT NULL,
-  constraint fk_authorities_users foreign key(username) references users(username)
+  constraint fk_authorities_users foreign key(username) references users(username) ON DELETE CASCADE
 );
 
 create unique index ix_auth_username on authorities(username, authority);
