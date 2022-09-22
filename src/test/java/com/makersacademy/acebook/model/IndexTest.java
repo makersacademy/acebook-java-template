ca1e26ad0fa7fddc4915c1c7075ab398f169ed10
Connor <swagdecon77@gmail.com>
2022-09-22 11:24:33 +0100

    all basic tests passing

--- src/test/java/com/makersacademy/acebook/model/IndexTest.java
+++ src/test/java/com/makersacademy/acebook/model/IndexTest.java
@@ -3,24 +3,20 @@ package com.makersacademy.acebook.model;
 import org.junit.Test;
 import com.github.javafaker.Faker;
 import com.makersacademy.acebook.Application;
-
-import java.sql.DriverManager;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By.ByCssSelector;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
-public class ErrorPageTest {
 
+public class IndexTest {
     WebDriver driver;
     Faker faker;
 
@@ -37,9 +33,25 @@ public class ErrorPageTest {
     }
 
     @Test
-    public void returns404error() {
-        driver.get("http://localhost:8080/qf3w");
-        String ExpectedErr = "OOOOOOOOOPS You done got 404.";
-        Assert.assertEquals(ExpectedErr, driver.findElement(By.tagName("body")).getText());
+    public void returnsPostsPageHTML() {
+        // Test Values
+        String RN = "Random Name";
+        String RP = "Random Password";
+
+        // Mocks Creating User
+        driver.get("http://localhost:8080/users/new");
+        driver.findElement(By.id("username")).sendKeys(RN);
+        driver.findElement(By.id("password")).sendKeys(RP);
+        driver.findElement(By.id("submit")).click();
+
+        // Mocks Logging in
+        driver.get("http://localhost:8080/login");
+        driver.findElement(By.id("username")).sendKeys(RN);
+        driver.findElement(By.id("password")).sendKeys(RP);
+        driver.findElement(By.className("btn")).click();
+
+        driver.get("http://localhost:8080/posts");
+        Assert.assertEquals("Content:",
+                driver.findElement(By.tagName("p")).getText());
     }
 }
