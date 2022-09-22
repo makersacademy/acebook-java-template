2a93e8cd081fb3793342c495c319d474b2de01f7
Connor <swagdecon77@gmail.com>
2022-09-22 12:45:16 +0100

    Another small change

--- src/test/java/com/makersacademy/acebook/model/ErrorPageTest.java
+++ src/test/java/com/makersacademy/acebook/model/ErrorPageTest.java
@@ -3,16 +3,12 @@ package com.makersacademy.acebook.model;
 import org.junit.Test;
 import com.github.javafaker.Faker;
 import com.makersacademy.acebook.Application;
-
-import java.sql.DriverManager;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By.ByCssSelector;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
