a9c6a11b1aff25e7056f002089c41aecf0ef02ad
Douglas Barco <douglas@dobuilding.co.uk>
2022-09-22 15:26:37 +0100

    Bcrypt working

--- src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
@@ -6,10 +6,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.sql.Driver;
 
 import javax.sql.DataSource;
 
@@ -19,11 +18,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource);
-    }
+    @Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// Enable jdbc authentication
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
+	}
+
+ 
+
+  
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,8 +43,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 // .logoutUrl("/logout").logoutSuccessUrl("/logout-success");
     }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
 }
