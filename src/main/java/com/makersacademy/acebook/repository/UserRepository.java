fde58430d3e0856c0162d04942562ec98a3420a7
BorisYTTsang <boris.yttsang@outlook.com>
2022-09-22 14:42:03 +0100

    Created friends table schema and seed for users table

--- src/main/java/com/makersacademy/acebook/repository/UserRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/UserRepository.java
@@ -8,5 +8,17 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-   User findByUsername(String username);
+  User findByUsername(String username);
+
+  public String getRequestStatus(int requesterID, int requesteeID) {
+
+  }
+  
+  public Iterable<User> getFriends(int requesterID) {
+
+  }
+
+  public Iterable<User> getFriendRequests(int requesterID, int requesteeID) {
+
+  }
 }
---
+++ src/main/resources/db/seeds/users_seed.sql
@@ -0,0 +1,15 @@
+TRUNCATE TABLE "users" RESTART IDENTITY CASCADE;
+
+INSERT INTO "users" ("username", "password", "enabled") VALUES
+('Benjamin', 'helloworld78', 'true'),
+('Chris', '12345678@', 'true'),
+('Sophia', 'helloworld78', 'true'),
+('Xiaoyu', 'tekkenrules99', 'true'),
+('Walter White', 'ihatemywife123', 'true'),
+('Timothy', 'timtamareyum123', 'true'),
+('Dave The Rave', 'illegalusernamelul55', 'true'),
+('Yasmin', 'QuEeNyAsMiN01', 'true'),
+('Scarlet', 'istomponurcorpse777', 'true'),
+('Aoife', 'itspronouncedeefa!!!', 'true'),
+('Eoin', 'itspronouncedowen!!!', 'true'),
+('Mario', 'itsamemario!', 'true');
