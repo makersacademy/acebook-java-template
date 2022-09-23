75843b33f01e9bd75d1be3eeb1228149909332f8
hakareem <harithakareem72@gmail.com>
2022-09-23 12:29:13 +0100

    AUTH

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -2,12 +2,17 @@ package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.repository.PostRepository;
+import com.makersacademy.acebook.repository.UserRepository;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+// import java.security.Principal,java.io.Serializable;
 
 import java.util.List;
 
@@ -18,7 +23,13 @@ public class PostsController {
     PostRepository repository;
 
     @GetMapping("/posts")
+
     public String index(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth);
+        System.out.println(auth.getPrincipal());
+        System.out.println(auth.getCredentials());
+        // System.out.println(auth.getDetails());
         Iterable<Post> posts = repository.findAll(Sort.by(Sort.Direction.DESC, "id")); // filtering by id
         model.addAttribute("posts", posts);
         model.addAttribute("post", new Post());
