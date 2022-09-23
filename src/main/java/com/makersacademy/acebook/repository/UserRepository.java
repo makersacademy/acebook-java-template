2ae01bbabbd5b176f0b7da1fad0298bc6a31245b
hakareem <harithakareem72@gmail.com>
2022-09-23 16:07:30 +0100

    find by username method

--- src/main/java/com/makersacademy/acebook/repository/UserRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByUsername(String username);
+
+    User findByUsername(String username);
 }
