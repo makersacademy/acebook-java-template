d0ea80ef6809f1a199c7d44f93a7c9ba3ef95a39
hakareem <harithakareem72@gmail.com>
2022-09-23 16:07:06 +0100

    posts controller

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -1,10 +1,13 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -12,8 +15,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-// import java.security.Principal,java.io.Serializable;
-
+import java.security.Principal;
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Controller
@@ -22,15 +28,27 @@ public class PostsController {
     @Autowired
     PostRepository repository;
 
-    @GetMapping("/posts")
+    @Autowired
+    UserRepository userRepository;
 
+    private Long getUserId() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        Long id = userRepository.findByUsername(authentication.getName()).getId();
+        return id;
+    }
+
+    private Timestamp getTimeStamp() {
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        return timestamp;
+    }
+
+    @GetMapping("/posts")
     public String index(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(auth);
-        System.out.println(auth.getPrincipal());
-        System.out.println(auth.getCredentials());
-        // System.out.println(auth.getDetails());
         Iterable<Post> posts = repository.findAll(Sort.by(Sort.Direction.DESC, "id")); // filtering by id
+        // User user = userRepository.findById(post.getUser_id()).get();
         model.addAttribute("posts", posts);
         model.addAttribute("post", new Post());
         return "posts/index";
@@ -38,7 +56,14 @@ public class PostsController {
 
     @PostMapping("/posts")
     public RedirectView create(@ModelAttribute Post post) {
+        // use the setter to store the user_id
+
+        post.setDate(this.getTimeStamp());
+        post.setUser_id(getUserId());
         repository.save(post);
+        System.out.println(post.getTimeString());
+        System.out.println(post.getDateString());
+        // System.out.println(post.getDateString());
         return new RedirectView("/posts");
     }
 }
