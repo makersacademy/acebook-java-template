28da909cde4ecf76ce18631e275b2a1c619eeb98
hakareem <harithakareem72@gmail.com>
2022-09-26 11:25:29 +0100

    sql query for likes

--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -1,15 +1,20 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.Like;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+ @Query(value = "SELECT COUNT(1) FROM likes WHERE post_id = ?1", nativeQuery = true)
+ Long findNumberOfLikesForAPost(Long id);
 }
