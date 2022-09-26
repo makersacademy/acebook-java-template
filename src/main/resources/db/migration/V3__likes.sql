7223068dc2ac4cbd2034e443761f79c76fad6132
hakareem <harithakareem72@gmail.com>
2022-09-26 15:12:55 +0100

    update likes table

--- src/main/resources/db/migration/V3__likes.sql
+++ src/main/resources/db/migration/V3__likes.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS likes;
 
 CREATE TABLE likes (
     id bigserial PRIMARY KEY,
-    user_id bigserial NOT NULL,
-    post_id bigserial NOT NULL,
-    constraint fk_likes_users foreign key(user_id) references users(id),
-    constraint fk_likes_posts foreign key(post_id) references posts(id),
-    UNIQUE(user_id, post_id)
+    userid bigserial NOT NULL,
+    postid bigserial NOT NULL,
+    constraint fk_likes_users foreign key(userid) references users(id),
+    constraint fk_likes_posts foreign key(postid) references posts(id),
+    UNIQUE(userid, postid)
 );
