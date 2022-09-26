65b7dae6ab4b756e1efd9406afb33ecc4cbc292d
hakareem <harithakareem72@gmail.com>
2022-09-26 17:29:37 +0100

    list of likes display

--- src/main/java/com/makersacademy/acebook/controller/LikesController.java
+++ src/main/java/com/makersacademy/acebook/controller/LikesController.java
@@ -1,6 +1,7 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Like;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.LikeRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 
@@ -32,16 +33,35 @@ public class LikesController {
         return id;
     }
 
-    // @GetMapping("/likes")
-    // public String like(Model model) {
-    // return "/likes";
-    // }
+    @GetMapping("/likes")
+    public String like(Model model) {
+        // Iterable<Like> likes = repository.findAll();
+        // // User user = userRepository.findById(like.getUserId().get());
+        // model.addAttribute("likes", likes);
+        // model.addAttribute("like", new Like());
+        return "likes/post";
+    }
 
-    // @PostMapping("/likes")
-    // public RedirectView like(@ModelAttribute Like like) {
-    // like.setUser_id(getUserId());
-    // repository.save(like);
+    @RequestMapping("/likes/post")
+    public String likeByPost(@RequestParam("postid") Long postid, Model model) {
+        List<Object[]> nlikes = repository.getUsersByPostid(postid);
+        System.out.println(nlikes);
+        try {
+            List<Object[]> likes = repository.getUsersByPostid(postid);
+            model.addAttribute("likes", likes);
 
-    // return new RedirectView("/posts");
-    // }
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+
+        return "likes/post";
+    }
 }
+
+// @PostMapping("/likes")
+// public RedirectView like(@ModelAttribute Like like) {
+// like.setUser_id(getUserId());
+// repository.save(like);
+
+// return new RedirectView("/posts");
+// }
--- src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
@@ -1,17 +1,19 @@
 package com.makersacademy.acebook.repository;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.makersacademy.acebook.model.Like;
+import com.makersacademy.acebook.model.User;
 
 @Transactional // the process of refunding an item
 public interface LikeRepository extends CrudRepository<Like, Long> {
 
  void deleteByUseridAndPostid(Long userid, Long postid);
 
- // @Query(value = "SELECT FROM likes where userid=?1 RETURNING *", nativeQuery =
- // true)
- // void deleteLikeByUserAndPostId(Long userid, Long postid);
+ @Query(value = "SELECT users.username FROM users INNER JOIN likes ON users.id = likes.userid WHERE likes.postid = ?1", nativeQuery = true)
+ List<Object[]> getUsersByPostid(Long postid);
 }
