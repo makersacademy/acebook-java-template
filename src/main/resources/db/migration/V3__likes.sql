c51e5d57e77b4e77e78c8eb99b8cf3e91e4e8b9e
hakareem <harithakareem72@gmail.com>
2022-09-26 14:45:10 +0100

    unique ids

--- src/main/resources/db/migration/V3__likes.sql
+++ src/main/resources/db/migration/V3__likes.sql
@@ -5,5 +5,6 @@ CREATE TABLE likes (
     user_id bigserial NOT NULL,
     post_id bigserial NOT NULL,
     constraint fk_likes_users foreign key(user_id) references users(id),
-    constraint fk_likes_posts foreign key(post_id) references posts(id)
+    constraint fk_likes_posts foreign key(post_id) references posts(id),
+    UNIQUE(user_id, post_id)
 );
