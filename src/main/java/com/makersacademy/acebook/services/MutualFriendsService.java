6227e91c60f1f615be2ab286e216f181f51b4ebc
BorisYTTsang <boris.yttsang@outlook.com>
2022-09-27 19:34:02 +0100

    -m "Think I finally fixed everything"

--- src/main/java/com/makersacademy/acebook/services/MutualFriendsService.java
+++ src/main/java/com/makersacademy/acebook/services/MutualFriendsService.java
@@ -11,19 +11,43 @@ import com.makersacademy.acebook.repository.UserRepository;
 @Service
 public class MutualFriendsService {
   
+  // Can use this service to store user(friend) ids for accessing outer loop variable in inner nested loop for checking blocked relationships
+  private String id;
+  private int count;
+
   @Autowired
   private UserRepository userRepository;
 
   public Iterable<User> getMutualFriends(String userId1, String userId2) {
-    Long Id1 = Long.valueOf(userId1);
-    Long Id2 = Long.valueOf(userId2);
-    return userRepository.getMutualFriends(Id1, Id2);
+    Long id1 = Long.valueOf(userId1);
+    Long id2 = Long.valueOf(userId2);
+    return userRepository.getMutualFriends(id1, id2);
   }
 
   public String getRequestStatus(String userId1, String userId2) {
-    Long Id1 = Long.valueOf(userId1);
-    Long Id2 = Long.valueOf(userId2);
-    return userRepository.getRequestStatus(Id1, Id2);
+    Long id1 = Long.valueOf(userId1);
+    Long id2 = Long.valueOf(userId2);
+    return userRepository.getRequestStatus(id1, id2);
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public int getCount() {
+    return this.count;
+  }
+
+  public void resetCount() {
+    this.count = 0;
+  }
+
+  public void incrementCount() {
+    this.count++;
   }
 
   public Iterable<User> findAll() {
