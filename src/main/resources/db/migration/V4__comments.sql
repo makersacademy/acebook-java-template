c5e86cc2d9b507d206a28aa00277a5d5911c745e
hakareem <harithakareem72@gmail.com>
2022-09-27 13:57:18 +0100

    comments table

--- src/main/resources/db/migration/V4__comments.sql
+++ src/main/resources/db/migration/V4__comments.sql
@@ -2,8 +2,12 @@ DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
     id bigserial PRIMARY KEY,
+    userid bigserial NOT NULL,
     postid bigserial, 
     content varchar(140) NOT NULL,
     date TIMESTAMP,
+    username VARCHAR(40),
+    constraint fk_comments_users foreign key(userid) references users(id),
     constraint fk_comments_post foreign key(postid) references posts(id)
+    -- UNIQUE(userid, postid)
 );
