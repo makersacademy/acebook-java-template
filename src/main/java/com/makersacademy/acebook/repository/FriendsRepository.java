e1e85b6ec2118488c5a905956346d4b049fa5cc0
hakareem <harithakareem72@gmail.com>
2022-09-28 17:38:03 +0100

    friends repo

--- src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
@@ -6,16 +6,21 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.makersacademy.acebook.model.Friend;
+import com.makersacademy.acebook.model.User;
 
 public interface FriendsRepository extends CrudRepository<Friend, Long> {
 
- @Query(value = "SELECT friends.request_to FROM friends WHERE request_from = ?1", nativeQuery = true)
+ @Query(value = "SELECT users.username, friends.request_to FROM users INNER JOIN friends ON users.id = friends.request_to WHERE request_from = ?1", nativeQuery = true)
  Iterable<BigInteger> pendingFriends(Long request_from);
 
  // @Query(value = "SELECT users.username, likes.formatted_date,
  // likes.formatted_time FROM users INNER JOIN likes ON users.id = likes.userid
  // WHERE likes.postid = ?1", nativeQuery = true)
 
- @Query(value = "SELECT friends.id request_id, users.username FROM users INNER JOIN friends ON users.id = friends.request_from WHERE friends.request_to = ?1", nativeQuery = true)
+ @Query(value = "SELECT friends.id request_id, users.username FROM users INNER JOIN friends ON users.id = friends.request_from WHERE friends.request_to = ?1 AND friends.status_code = FALSE", nativeQuery = true)
  Iterable<Object[]> pendingFriendsTo(Long request_to);
+
+ @Query(value = "SELECT users.username FROM users INNER JOIN friends ON users.id = friends.request_from WHERE friends.status_code = TRUE AND friends.request_to = 6 UNION SELECT users.username FROM users INNER JOIN friends ON users.id = friends.request_to WHERE friends.status_code = TRUE AND friends.request_from = 6", nativeQuery = true)
+ Iterable<Object[]> findFriends(Long userid);
+
 }
