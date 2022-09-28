c23a389951a9c3e7c43123abe5aa04f4a89a3aad
hakareem <harithakareem72@gmail.com>
2022-09-28 08:08:49 +0100

    directory exposure

--- src/main/java/com/makersacademy/acebook/controller/OverrideSpring.java
+++ src/main/java/com/makersacademy/acebook/controller/OverrideSpring.java
@@ -3,13 +3,34 @@ package com.makersacademy.acebook.controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class OverrideSpring implements WebMvcConfigurer {
+ @Override
+ public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  exposeDirectory("src/main/resources/static/image", registry);
+ }
+
+ private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+  Path uploadDir = Paths.get(dirName);
+  String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+  if (dirName.startsWith("../"))
+   dirName = dirName.replace("../", "");
+
+  registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/" + uploadPath + "/");
+ }
 
  @Override
  public void addViewControllers(ViewControllerRegistry registry) {
   registry.addViewController("/login").setViewName("login");
   registry.addViewController("/logout").setViewName("logout");
  }
+
 }
