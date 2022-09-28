94f81557b152428ebceb13d970a97288fb047cf0
hakareem <harithakareem72@gmail.com>
2022-09-28 09:41:44 +0100

    likes repo

--- src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.makersacademy.acebook.model.Like;
