2cae1c1c278d25a398e86c7f41cd57e842475ddb
hakareem <harithakareem72@gmail.com>
2022-09-28 13:06:19 +0100

    sql queries

--- src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
@@ -1,5 +1,6 @@
 package com.makersacademy.acebook.repository;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -7,6 +8,14 @@ import org.springframework.data.repository.CrudRepository;
 import com.makersacademy.acebook.model.Friend;
 
 public interface FriendsRepository extends CrudRepository<Friend, Long> {
- @Query(value = "select exists (SELECT * FROM friends WHERE request_from = ?1 AND request_to = ?2 AND status_code = FALSE)", nativeQuery = true)
- boolean requestSent(Long user_from, Long user_to);
+
+ @Query(value = "SELECT friends.request_to FROM friends WHERE request_from = ?1", nativeQuery = true)
+ Iterable<BigInteger> pendingFriends(Long request_from);
+
+ // @Query(value = "SELECT users.username, likes.formatted_date,
+ // likes.formatted_time FROM users INNER JOIN likes ON users.id = likes.userid
+ // WHERE likes.postid = ?1", nativeQuery = true)
+
+ @Query(value = "SELECT friends.id request_id, users.username FROM users INNER JOIN friends ON users.id = friends.request_from WHERE friends.request_to = ?1", nativeQuery = true)
+ Iterable<Object[]> pendingFriendsTo(Long request_to);
 }
