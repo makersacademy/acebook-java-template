90e8ae1ea7f89a7bcd6f1b2ceaa14a66607cadf9
hakareem <harithakareem72@gmail.com>
2022-09-28 11:06:04 +0100

    friends repo

--- src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
@@ -1,8 +1,12 @@
 package com.makersacademy.acebook.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.makersacademy.acebook.model.Friend;
 
 public interface FriendsRepository extends CrudRepository<Friend, Long> {
-
+ @Query(value = "select exists (SELECT * FROM friends WHERE request_from = ?1 AND request_to = ?2 AND status_code = FALSE)", nativeQuery = true)
+ boolean requestSent(Long user_from, Long user_to);
 }
