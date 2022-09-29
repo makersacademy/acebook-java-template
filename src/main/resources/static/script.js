a6901f8f88efb393e65485a073bab4451ac24c1e
BorisYTTsang <boris.yttsang@outlook.com>
2022-09-29 16:06:30 +0100

    :m "Search bar done"

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -64,3 +64,19 @@ function getSearchDropdown() {
     setTimeout(show, 120);
   // }
 }
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("searchDropdown");
+  a = div.getElementsByClassName("searchwrapper");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
