c09cc16efd4a4c88038a1ffd110a167048e659e3
hakareem <harithakareem72@gmail.com>
2022-09-29 08:52:04 +0100

    friends table

--- src/main/resources/db/migration/V5__friends.sql
+++ src/main/resources/db/migration/V5__friends.sql
@@ -5,5 +5,6 @@ CREATE TABLE friends (
     status_code varchar(50) NOT NULL,
     request_from bigserial,
     request_to bigserial,
-    sent_at TIMESTAMP
+    sent_at TIMESTAMP,
+    UNIQUE(request_from, request_to)
 );
