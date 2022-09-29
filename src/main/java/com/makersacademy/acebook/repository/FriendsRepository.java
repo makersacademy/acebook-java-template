166d838e6f58b9d7779f70d919b8189556cfd36c
hakareem <harithakareem72@gmail.com>
2022-09-29 14:52:24 +0100

    friends repo

--- src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendsRepository.java
@@ -2,7 +2,6 @@ package com.makersacademy.acebook.repository;
 
 import java.math.BigInteger;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.makersacademy.acebook.model.Friend;
@@ -20,8 +19,13 @@ public interface FriendsRepository extends CrudRepository<Friend, Long> {
  @Query(value = "SELECT friends.id request_id, users.username FROM users INNER JOIN friends ON users.id = friends.request_from WHERE friends.request_to = ?1 AND friends.status_code = FALSE", nativeQuery = true)
  Iterable<Object[]> pendingFriendsTo(Long request_to);
 
- @Query(value = "SELECT users.username FROM users INNER JOIN friends ON users.id = friends.request_from WHERE friends.status_code = TRUE AND friends.request_to = ?1 UNION SELECT users.username FROM users INNER JOIN friends ON users.id = friends.request_to WHERE friends.status_code = TRUE AND friends.request_from = ?1", nativeQuery = true)
- Iterable<Object[]> findFriends(Long userid);
+ // @Query(value = "SELECT users.id, users.username FROM users INNER JOIN friends
+ // ON users.id = friends.request_from WHERE friends.status_code = TRUE AND
+ // friends.request_to = ?1 UNION SELECT users.id, users.username FROM users
+ // INNER JOIN friends ON users.id = friends.request_to WHERE friends.status_code
+ // = TRUE AND friends.request_from = ?1", nativeQuery = true)
+ // Iterable<User> findFriends(Long userid);
 
  void deleteById(Long requestId);
+
 }
