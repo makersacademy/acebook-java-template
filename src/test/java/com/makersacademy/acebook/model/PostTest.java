259eac277861a76eb0527f9f92d713b22ac4aa36
Asimina Athanasopoulou <a.athanasopoulou@elsevier.com>
2022-11-15 12:06:30 +0000

    'Write a feature test' subtask: Initial commit - Timestamp imported

--- src/test/java/com/makersacademy/acebook/model/PostTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostTest.java
@@ -2,16 +2,28 @@ package com.makersacademy.acebook.model;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
-
+import java.sql.Timestamp;
+import javax.persistence.Column;
 import org.junit.Test;
 
 public class PostTest {
 
+	@Data
+	@Entity
+	@Table(name = "posts")
+
 	private Post post = new Post("hello", 0);
+
+	@Column(name="time_created")			//database column name? (time_created?)
+	private Timestamp time_created;
+
+	public void setTimeCreated(Timestamp time_created) {this.time_created = time_created; }
+
 
 	@Test
 	public void postHasContent() {
 		assertThat(post.getContent(), containsString("hello"));
 	}
+
 
 }
