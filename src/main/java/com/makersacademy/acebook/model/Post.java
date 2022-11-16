6f10616c45a953bc31cfe63fecc41683ce7309a1
Danny W <wangodanny@gmail.com>
2022-11-16 11:33:47 +0000

    updated files to fix bigint error

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -1,5 +1,8 @@
 package com.makersacademy.acebook.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
