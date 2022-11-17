b2b04c24b55dee49b21ccf3351892f2327c00a9d
Ella Skolnick <ellaskolnick@me.com>
2022-11-17 11:22:55 +0000

    post tests up to date

--- src/test/java/com/makersacademy/acebook/model/PostTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostTest.java
@@ -3,6 +3,8 @@ package com.makersacademy.acebook.model;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 public class PostTest {
@@ -12,6 +14,19 @@ public class PostTest {
 	@Test
 	public void postHasContent() {
 		assertThat(post.getContent(), containsString("hello"));
+	}
+
+  @Test
+	public void setPostDate() {
+    Date date = new Date();
+    post.setTime_posted(date);
+		assertEquals(post.getTime_posted(), date);
+	}
+
+  @Test
+	public void setPostUser() {
+    post.setUser_id(1);
+		assertEquals(post.getUser_id(), Integer.valueOf(1));
 	}
 
 }
