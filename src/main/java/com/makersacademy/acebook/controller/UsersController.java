99fc5b024c504d7f1dc6f171b1c840bb158c878e
deborah <deborahnwankwo1998@gmail.com>
2022-11-18 14:43:15 +0000

    added upload photo feature to sign up and photo icon with each post

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -4,12 +4,19 @@ import com.makersacademy.acebook.model.Authority;
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.AuthoritiesRepository;
 import com.makersacademy.acebook.repository.UserRepository;
+
+import antlr.StringUtils;
+
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
