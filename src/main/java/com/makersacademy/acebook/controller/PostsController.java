f275e14eae0459de60ddcde33552818e19e933ad
giorgio <giorgio.gristina91@gmail.com>
2022-11-18 17:19:20 +0000

    we have finally displayed the data of the post in the show page

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -1,6 +1,6 @@
 package com.makersacademy.acebook.controller;
 
-import com.makersacademy.acebook.model.Comment;
+
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.PostRepository;
@@ -44,10 +44,10 @@ public class PostsController {
     @GetMapping("/posts/{id}")
     public String show(@PathVariable Long id, Model model){
         Optional<Post> objPost = postRepository.findById(id);
-      
-        model.addAttribute("posts", objPost);
-        // model.addAttribute("post", new Post());
-       System.out.println( model.addAttribute("post", new Post()));
+        Post post = objPost.get();
+        System.out.println(post);
+        model.addAttribute("posts", post);
+        model.addAttribute("post", new Post());
         return "posts/show";
     }
 
