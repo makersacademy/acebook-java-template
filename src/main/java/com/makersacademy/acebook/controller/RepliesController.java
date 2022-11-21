890cacd0b4cbf8b25a6e18d1b65d1b207f139f8b
Ali <alibcomputerscience@gmail.com>
2022-11-21 12:12:32 +0000

    Complete redesign of html and css  + added functionality for buttons via javascript and added usernames to replies database

--- src/main/java/com/makersacademy/acebook/controller/RepliesController.java
+++ src/main/java/com/makersacademy/acebook/controller/RepliesController.java
@@ -3,8 +3,12 @@ package com.makersacademy.acebook.controller;
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.model.Reply;
 import com.makersacademy.acebook.repository.ReplyRepository;
+import com.makersacademy.acebook.repository.UserRepository;
 
+import java.security.Principal;
 import java.util.Date;
+import java.util.Optional;
+import com.makersacademy.acebook.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,11 +22,22 @@ import org.springframework.web.servlet.view.RedirectView;
 public class RepliesController {
     @Autowired
     ReplyRepository repository;
+    @Autowired
+    UserRepository urepository;
 
     @PostMapping("/posts/reply")
-    public RedirectView createReply(@ModelAttribute Reply reply) {
+    public RedirectView createReply(@ModelAttribute Reply reply, Principal principal) {
         Date date = new Date();
+        String userName = principal.getName();
+        Optional<User> currentUser = urepository.findByUsername(userName);
+        User user = currentUser.get();
+        Long userIdLong = user.getId();
+        Integer userId = userIdLong.intValue();
+
         reply.setTime_posted(date);
+        reply.setUser_id(userId);
+        //reply.setUsername(userName);
+
         repository.save(reply);
 
         return new RedirectView("/posts");
--- src/main/resources/db/migration/V7__create_reply_table.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS replies;
-
-CREATE TABLE replies (
-  id bigserial PRIMARY KEY,
-  content varchar(250) NOT NULL,
-  post_id INTEGER REFERENCES posts(id),
-  constraint fk_posts_id foreign key (post_id) REFERENCES posts(id)
-);
