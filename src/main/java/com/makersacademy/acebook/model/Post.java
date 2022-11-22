2d64f039b38b288b45980fb0de26a2fe05d4ed26
Ilyas Sung <ilyas@192.168.1.219>
2022-11-22 12:18:55 +0000

    Fixed deleteing posts with likes or replies

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -1,10 +1,13 @@
 package com.makersacademy.acebook.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.GenerationType;
 
@@ -23,6 +26,12 @@ public class Post {
     private Integer userId;
     private String username;
     private String image;
+
+    @OneToMany(mappedBy = "post_id", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reply> replies;
+
+    @OneToMany(mappedBy = "post_id", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes;
 
     public Post() {}
 
