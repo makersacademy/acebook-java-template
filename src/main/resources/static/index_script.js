b7547a9dde653614f34594c02a1861b33f6d9102
Owen Alistair Steel <woofdog04@gmail.com>
2022-11-22 12:54:23 +0000

    adds reply function to post view page, refactors script

--- src/main/resources/static/index_script.js
+++ src/main/resources/static/index_script.js
@@ -1,24 +1,24 @@
 
-const inputBox = document.getElementsByClassName("main-input")[0];
 
-const mainPostBtn = document.getElementsByClassName("main-btn")[0];
+if(document.getElementsByClassName("main-btn").length > 0 && document.getElementsByClassName("main-input").length > 0){
+    const inputBox = document.getElementsByClassName("main-input")[0];
 
-mainPostBtn.style.background = "buttonface";
-mainPostBtn.disabled = 'true';
+    const mainPostBtn = document.getElementsByClassName("main-btn")[0];
+    mainPostBtn.style.background = "buttonface";
+    mainPostBtn.disabled = 'true';
 
+    inputBox.addEventListener('input', output);
+    function output(e) {
+        console.log(mainPostBtn);
+        if (e.target.value == "") {
+            mainPostBtn.style.background = "buttonface";
+            mainPostBtn.disabled = 'true';
+        }else {
+            mainPostBtn.style.background = "buttonface";
+            mainPostBtn.removeAttribute("disabled");
+        }
 
-inputBox.addEventListener('input', output);
-
-function output(e) {
-    console.log(mainPostBtn);
-    if (e.target.value == "") {
-        mainPostBtn.style.background = "buttonface";
-        mainPostBtn.disabled = 'true';
-    }else {
-        mainPostBtn.style.background = "buttonface";
-        mainPostBtn.removeAttribute("disabled");
     }
-
 }
 
 
@@ -31,7 +31,9 @@ for (let i =0; i < replybuttons.length;i++) {
 }
 
 function reply_btn(e) {
-    e.currentTarget.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("reply")[0].style.display = 'flex';
+    const ReplyWrapper = e.currentTarget.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("reply")[0];
+    ReplyWrapper.style.display = 'flex';
+    ReplyWrapper.querySelector(".main-input").focus();
 }
 
 //console.log(replybuttons);
