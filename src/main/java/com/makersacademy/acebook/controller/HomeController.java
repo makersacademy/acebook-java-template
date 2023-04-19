23fef3701dd43549aa4b553c4fd0d4fd2bdfebe7
raymond anderson <raymond.anderson84@hotmail.co.uk>
2023-04-19 14:24:14 +0100

    Custom login pages

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -3,11 +3,26 @@ package com.makersacademy.acebook.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/")
 	public RedirectView index() {
 		return new RedirectView("/posts");
+	}
+	@RequestMapping("/login")
+	public String login() {
+		return "login.html";
+	}
+	@RequestMapping("/logout")
+	public String logout() {
+		return "logout.html";
+	}
+	// Login form with error
+	@RequestMapping("/login-error")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login.html";
 	}
 }
