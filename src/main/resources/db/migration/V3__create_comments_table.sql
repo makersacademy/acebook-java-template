3121628cd2cdecaca69a97537588fe00580cd652
laibaali500 <alilaiba006@gmail.com>
2023-11-14 14:12:04 +0000

    Fix database id types

--- src/main/resources/db/migration/V3__create_comments_table.sql
+++ src/main/resources/db/migration/V3__create_comments_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
   id bigserial PRIMARY KEY,
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  post_id INT NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+  user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  post_id BIGINT NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
   content varchar(250) NOT NULL
 );
