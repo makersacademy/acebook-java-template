dddb8bcd3b292f06fc2e1503fcb163f4a224b90c
laibaali500 <alilaiba006@gmail.com>
2023-11-14 14:09:19 +0000

    Add Comment Repo

--- src/main/java/com/makersacademy/acebook/repository/CommentRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/CommentRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
-    public List<Comment> findCommentByPost_id(Long post_id);
+    //public List<Comment> findCommentByPost_id(Long post_id);
+    // ^ idk why but this doesn't work
 }
