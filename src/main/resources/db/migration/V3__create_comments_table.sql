43c6de0a8718f0535d409bb76aac892743c3661c
laibaali500 <alilaiba006@gmail.com>
2023-11-15 10:00:28 +0000

    Update FK in comments from user_id to username

--- src/main/resources/db/migration/V3__create_comments_table.sql
+++ src/main/resources/db/migration/V3__create_comments_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
   id bigserial PRIMARY KEY,
-  user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  username varchar(50) NOT NULL REFERENCES users(username) ON DELETE CASCADE,
   post_id BIGINT NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
   content varchar(250) NOT NULL
 );
