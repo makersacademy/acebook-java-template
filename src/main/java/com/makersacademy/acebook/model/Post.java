d69b24d23da36fa181d665dcc6b5715a07762120
laibaali500 <alilaiba006@gmail.com>
2023-11-16 11:45:23 +0000

    Add getter for id attribute

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -23,6 +23,7 @@ public class Post {
     public Post(String content) {
         this.content = content;
     }
+    public Long getId() {return this.id; }
     public String getContent() { return this.content; }
     public void setContent(String content) { this.content = content; }
 
