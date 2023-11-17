a39da62c493a8e32ca012d46ce2e8c4da18a0633
laibaali500 <alilaiba006@gmail.com>
2023-11-17 14:11:09 +0000

    Add test to verify that posts store comments

--- src/test/java/com/makersacademy/acebook/model/CommentTest.java
+++ src/test/java/com/makersacademy/acebook/model/CommentTest.java
@@ -5,14 +5,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class CommentTest {
 
     private Post post = new Post("This is a post.");
-    private Comment comment = new Comment("Ariane", post.getId(), "I like your post!");
+    private Comment comment1 = new Comment("Ariane", post.getId(), "I like your post!");
+    private Comment comment2 = new Comment("Bob", post.getId(), "Your post is cringe!");
 
     @Test
     public void commentHasContent() {
-        assertThat(comment.getContent(), containsString("I like your post!"));
+        assertThat(comment1.getContent(), containsString("I like your post!"));
+    }
+
+    @Test
+    public void postStoresComments() {
+        List<Comment> comments = new ArrayList<Comment>();
+        comments.add(comment1);
+        comments.add(comment2);
+        post.setComments(comments);
+        assertEquals(post.getComments(), comments);
     }
 
 }
