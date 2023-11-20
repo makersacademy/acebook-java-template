d29d881d985b1283711d5ade188cd1e6423dbe35
Zakarie <zakariaali350@gmail.com>
2023-11-20 12:04:22 +0000

    display names of request users with buttons

--- src/main/java/com/makersacademy/acebook/controller/FriendController.java
+++ src/main/java/com/makersacademy/acebook/controller/FriendController.java
@@ -3,6 +3,7 @@ package com.makersacademy.acebook.controller;
 import com.makersacademy.acebook.model.Friend;
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.FriendRepository;
+import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,16 +11,54 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 @Controller
 public class FriendController {
 
     @Autowired
     FriendRepository friendRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/users/friend")
     public ModelAndView showFriends(Model model) {
         ModelAndView modelAndView = new ModelAndView("/users/friend");
         modelAndView.addObject("friend", new Friend());
+        return modelAndView;
+    }
+
+    @GetMapping("/friends")
+    public ModelAndView showRequests(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("/users/friends");
+
+        Optional<User> currentUser = userRepository.findByUsername(principal.getName());
+        User principalUser = currentUser.orElse(null);
+
+        assert principalUser != null;
+        List<Friend> requests = friendRepository.findByStatus("PENDING");
+        ArrayList<Friend> userRequests = new ArrayList<>();
+
+        for (Friend friend : requests) {
+            if (Objects.equals(friend.getReceiver_id(), principalUser.getId())) {
+                userRequests.add(friend);
+            }
+        }
+
+        ArrayList<User> friendRequests = new ArrayList<>();
+
+        for (Friend friend : userRequests) {
+            Optional<User> optionalUser = userRepository.findById(friend.getRequester_id());
+            User friendRequest = optionalUser.orElse(null);
+            friendRequests.add(friendRequest);
+        }
+
+        modelAndView.addObject("friendRequests", friendRequests);
+        System.out.println(friendRequests);
         return modelAndView;
     }
 
--- src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
@@ -3,6 +3,9 @@ package com.makersacademy.acebook.repository;
 import com.makersacademy.acebook.model.Friend;
 import org.springframework.data.repository.CrudRepository;
 
-public interface FriendRepository extends CrudRepository<Friend, Long>  {
+import java.util.List;
+import java.util.Optional;
 
+public interface FriendRepository extends CrudRepository<Friend, Long>  {
+    List<Friend> findByStatus(String status);
 }
