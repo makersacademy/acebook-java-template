31d5e83246abe631bd0d6526eefe383690c797fe
EthanDunwiddie <ethan.dunwiddie@gmail.com>
2024-06-11 10:56:03 +0100

    added username to posts

--- src/test/java/com/makersacademy/acebook/model/PostTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostTest.java
@@ -2,16 +2,30 @@ package com.makersacademy.acebook.model;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PostTest {
 
-	private Post post = new Post("hello");
+	private User user;
+	private Post post;
+
+	@Before
+	public void setUp(){
+		user = mock(User.class);
+		when(user.getUsername()).thenReturn("testuser");
+		post = new Post("hello", user);
+	}
 
 	@Test
 	public void postHasContent() {
 		assertThat(post.getContent(), containsString("hello"));
 	}
 
+	@Test
+	public void postHasUser() {
+		assertEquals("testuser", post.getUser().getUsername());
+	}
 }
