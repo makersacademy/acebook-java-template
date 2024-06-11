2a391e0b27a4e09fab88e025ed95a714011919b8
neily2nd <neildavison1985@hotmail.com>
2024-06-11 17:03:23 +0100

    Implemented Edit method and created test.
    Needs work done on design.
    
    AGAIN! DO NOT DELETE!!!!!111one

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -41,3 +41,18 @@
 //    }
 //}
 
+//    @After
+//    public void tearDown() {
+//        driver.close();
+//    }
+//
+//    @Test
+//    public void successfulSignUpRedirectsToSignIn() {
+//        driver.get("http://localhost:8080/users/new");
+//        driver.findElement(By.id("username")).sendKeys(faker.name().firstName());
+//        driver.findElement(By.id("password")).sendKeys("password");
+//        driver.findElement(By.id("submit")).click();
+//        String title = driver.getTitle();
+//        Assert.assertEquals("Please sign in", title);
+//    }
+//}
