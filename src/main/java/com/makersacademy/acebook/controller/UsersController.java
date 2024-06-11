4d35d70f6982740f8bf12589b047f3c93ad0dd03
EthanDunwiddie <ethan.dunwiddie@gmail.com>
2024-06-11 13:20:05 +0100

    User can add profile picture on signup

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -10,7 +10,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class UsersController {
@@ -20,6 +27,8 @@ public class UsersController {
     @Autowired
     AuthoritiesRepository authoritiesRepository;
 
+    private static final String UPLOAD_DIR = "src/main/resources/static/profile-pictures/";
+
     @GetMapping("/users/new")
     public String signup(Model model) {
         model.addAttribute("user", new User());
@@ -27,10 +36,21 @@ public class UsersController {
     }
 
     @PostMapping("/users")
-    public RedirectView signup(@ModelAttribute User user) {
-        userRepository.save(user);
-        Authority authority = new Authority(user.getUsername(), "ROLE_USER");
-        authoritiesRepository.save(authority);
+    public RedirectView signup(@ModelAttribute User user, @RequestParam("profilePicture") MultipartFile file) {
+        try{
+            if(!file.isEmpty()){
+                String filename = file.getOriginalFilename();
+                Path path = Paths.get(UPLOAD_DIR + filename);
+                Files.createDirectories(path.getParent());
+                Files.write(path, file.getBytes());
+                user.setProfilePictureUrl("/profile-pictures/" + UPLOAD_DIR + filename);
+            }
+            userRepository.save(user);
+            Authority authority = new Authority(user.getUsername(), "ROLE_USER");
+            authoritiesRepository.save(authority);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
         return new RedirectView("/login");
     }
 }
--- src/main/java/com/makersacademy/acebook/model/User.java
+++ src/main/java/com/makersacademy/acebook/model/User.java
@@ -1,12 +1,9 @@
 package com.makersacademy.acebook.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import static java.lang.Boolean.TRUE;
 
@@ -20,25 +17,35 @@ public class User {
     private String username;
     private String password;
     private boolean enabled;
+    private String profilePictureUrl;
+
+    @Transient
+    private MultipartFile profilePicture;
 
     public User() {
         this.enabled = TRUE;
     }
 
-    public User(String username, String password) {
+    public User(String username, String password, String profilePictureUrl) {
         this.username = username;
         this.password = password;
         this.enabled = TRUE;
+        this.profilePictureUrl = profilePictureUrl;
     }
 
-    public User(String username, String password, boolean enabled) {
+    public User(String username, String password, boolean enabled, String profilePictureUrl) {
         this.username = username;
         this.password = password;
         this.enabled = enabled;
+        this.profilePictureUrl = profilePictureUrl;
     }
 
     public String getUsername() { return this.username; }
     public String getPassword() { return this.password; }
+    public MultipartFile getProfilePicture() { return this.profilePicture; }
+    public String getProfilePictureUrl() { return this.profilePictureUrl; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
+    public void setProfilePicture(MultipartFile profilePicture) { this.profilePicture = profilePicture; }
+    public void setProfilePictureUrl(String profilePictureUrl) { this.profilePictureUrl = profilePictureUrl; }
 }
