e78abf3efb5cfad5b2106e6b244c68c506066d70
Shoaib Latif <shoaiblatif11@gmail.com>
2024-06-12 16:48:03 +0100

    Added in test for posts and comments. Both tests pass successfully

---
+++ src/test/java/com/makersacademy/acebook/model/Controller/commentTests/CommentsTest.java
@@ -0,0 +1,79 @@
+package com.makersacademy.acebook.model.Controller.commentTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.github.javafaker.Faker;
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+
+
+
+public class CommentsTest {
+    WebDriver driver;
+    Faker faker;
+    User user;
+
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        driver = new ChromeDriver();
+        faker = new Faker();
+        user = new User();
+        user.setUsername(faker.name().firstName());
+
+        driver.get("http://localhost:8080/users/new");
+        driver.findElement(By.id("username")).sendKeys(user.getUsername());
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.id("submit")).click();
+        driver.get("http://localhost:8080/login");
+        driver.findElement(By.id("username")).sendKeys(user.getUsername());
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.tagName("button")).click();
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+@Test
+    public void createComment() {
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    //Find the post and enter content
+    driver.findElement(By.id("content")).sendKeys("Hello this is a post");
+    //Click the submit button
+    driver.findElement(By.id("post-content-submit")).click();
+
+    WebElement post = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='post-container mt-4']//p[text()='Hello this is a post']")));
+
+    WebElement form = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='form-group']//input[contains(@name, 'commentText')]")));
+
+    form.sendKeys("This is a comment");
+
+    form.submit();
+
+    //Check if the comment was created
+    WebElement commentList = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='comment-container mt-2']//ul[@id='commentsList']")));
+    assert commentList.getText().contains("This is a comment");
+
+    }
+
+}
+
+
+
--- target/classes/db/migration/V4__alter_table_comments_to_include_post_id.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE comments
-ADD COLUMN post_id INT,
-ADD CONSTRAINT fk_post_id
-FOREIGN KEY (post_id)
-REFERENCES posts(id);
