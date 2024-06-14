73814315ad03ea54bf23f5c79a979eb4fbd6ad11
Niki Stefanopoulou <nikistefanopoulou@nikis-air.home>
2024-06-14 12:19:40 +0100

    Tests added for comment and friend model

--- src/test/java/com/makersacademy/acebook/model/CommentTest.java
+++ src/test/java/com/makersacademy/acebook/model/CommentTest.java
@@ -2,22 +2,40 @@ package com.makersacademy.acebook.model;
 
 
 import org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Timestamp;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class CommentTest {
 
-    private Comment comment = new Comment("This is a comment", 1L, 1L, new Timestamp(10000));
+    private Comment comment;
+
+    @Before
+    public void setUp() {
+        comment = new Comment("This is a comment", 1L, 1L, new Timestamp(10000));
+    }
 
     @Test
     public void commentHasContent() {
 
         assertThat(comment.getContent(), containsString("This is a comment"));
     }
-
+    @Test
+    public void commentHasUserId() {
+        assertEquals(Long.valueOf(1L), comment.getPostId());
+    }
+    @Test
+    public void commentHasPostId() {
+        assertEquals(Long.valueOf(1L), comment.getPostId());
+    }
+    @Test
+    public void commentHasTimestamp() {
+        assertEquals(new Timestamp(10000), comment.getCreatedAt());
+    }
 
 }
