457ca0939ee4e5cd59e9271f477c7155c3890a8e
Shoaib Latif <shoaiblatif11@gmail.com>
2024-06-14 11:17:39 +0100

    Fixed test

--- src/test/java/com/makersacademy/acebook/model/Controller/userTests/NavigatesToLoginAfterUsersControllerTest.java
+++ src/test/java/com/makersacademy/acebook/model/Controller/userTests/NavigatesToLoginAfterUsersControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -38,13 +40,16 @@ public class NavigatesToLoginAfterUsersControllerTest {
 
     @Test
     public void navigatesToLoginAfterUsersController() throws IOException {
-        driver.get("http://localhost:8080/users/new");
-        driver.findElement(By.id("username")).sendKeys(user.getUsername());
-        driver.findElement(By.id("password")).sendKeys("password");
-        driver.findElement(By.id("submit")).click();
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            driver.get("http://localhost:8080/users/new");
+            driver.findElement(By.id("username")).sendKeys(user.getUsername());
+            driver.findElement(By.id("password")).sendKeys("password");
+            driver.findElement(By.id("submit")).click();
 
-        String url = driver.getCurrentUrl();
-        assertEquals("http://localhost:8080/login", url);
+            wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe("http://localhost:8080/users/new")));
+
+            String url = driver.getCurrentUrl();
+            assertEquals("http://localhost:8080/login", url);
 
     }
 }
