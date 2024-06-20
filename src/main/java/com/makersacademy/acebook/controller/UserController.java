b3e57fa78d685d252c694fb2b70b620494bc322b
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2024-06-20 15:04:44 +0100

    add login-signup css

--- src/main/java/com/makersacademy/acebook/controller/UserController.java
+++ src/main/java/com/makersacademy/acebook/controller/UserController.java
@@ -5,10 +5,11 @@ import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-
+@CrossOrigin
 @Controller
 public class UserController {
 
