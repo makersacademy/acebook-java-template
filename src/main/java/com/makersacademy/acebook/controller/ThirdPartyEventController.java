48cdd280110694acb02a98d0769cecad72ac8e89
Rachel Roberts <rachel853roberts@gmail.com>
2024-06-21 16:56:20 +0100

    link fixed, thumbnail added, date formatted, API key env.var utilised

--- src/main/java/com/makersacademy/acebook/controller/ThirdPartyEventController.java
+++ src/main/java/com/makersacademy/acebook/controller/ThirdPartyEventController.java
@@ -12,50 +12,20 @@ import java.util.concurrent.ExecutionException;
 
 @Controller
 
-
-//public class MovieController {
-//
-//    @Autowired
-//    private ThirdPartyEventService movieService;
-//
-//    @GetMapping("/search")
-//    public String searchEvent(Model model) {
-//        try {
-//            List<ThirdPartyEvent> movies = movieService.searchMovie("Incep").get();
-//            model.addAttribute("movies", movies);
-//        } catch (InterruptedException | ExecutionException e) {
-//            e.printStackTrace();
-//            model.addAttribute("movies", List.of());
-//        }
-//        return "index";
-//    }
-//}
-
 public class ThirdPartyEventController {
 
     @Autowired
     private ThirdPartyEventService thirdPartyEventService;
 
-    @GetMapping("/search")
+    @GetMapping("/events")
     public String searchEvent(Model model) {
         try {
-            List<ThirdPartyEvent> events = thirdPartyEventService.searchEvent().get();
-            model.addAttribute("events", events);
+            List<ThirdPartyEvent> thirdPartyEvents = thirdPartyEventService.searchEvent().get();
+            model.addAttribute("thirdPartyEvents", thirdPartyEvents);
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
-            model.addAttribute("events", List.of());
+            model.addAttribute("thirdPartyEvents", List.of());
         }
         return "index";
     }
 }
-//
-//AsyncHttpClient client = new DefaultAsyncHttpClient();
-//client.prepare("GET", "https://real-time-events-search.p.rapidapi.com/search-events?query=Newcastle%2C%20UK&start=0")
-//	.setHeader("x-rapidapi-key", "330a9e01edmsh3544e9d43e6ff69p17ee8ejsnfee0e591ba76")
-//	.setHeader("x-rapidapi-host", "real-time-events-search.p.rapidapi.com")
-//	.execute()
-//	.toCompletableFuture()
-//	.thenAccept(System.out::println)
-//	.join();
-//
-//client.close();
