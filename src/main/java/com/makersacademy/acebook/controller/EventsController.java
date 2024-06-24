95d457a946af0b636a139d45f0c5669249310283
Umi007 <umihussain13@gmail.com>
2024-06-24 14:35:01 +0100

    debugging

--- src/main/java/com/makersacademy/acebook/controller/EventsController.java
+++ src/main/java/com/makersacademy/acebook/controller/EventsController.java
@@ -7,13 +7,14 @@ import com.makersacademy.acebook.repository.UserRepository;
 import com.makersacademy.acebook.service.CommentService;
 import com.makersacademy.acebook.service.ThirdPartyEventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import java.util.Date;
 import java.util.List;
@@ -76,34 +77,6 @@ public class EventsController {
             // Handle the case where the event is not found
             return "redirect:/error";
         }
-    }
-
-    @GetMapping("/")
-    public String userEvents(Model model,
-                             @AuthenticationPrincipal Object principal,
-                             @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date minScheduledDate,
-                             @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date maxScheduledDate) {
-        String username;
-        List<Event> events;
-
-        if (principal instanceof UserDetails) {
-            username = ((UserDetails) principal).getUsername();
-        } else if (principal instanceof OAuth2User) {
-            username = ((OAuth2User) principal).getAttribute("name");
-        } else {
-            username = "User";
-        }
-
-        model.addAttribute("name", username);
-
-        if (minScheduledDate != null && maxScheduledDate != null) {
-            events = eventRepository.findByScheduledDateBetween(minScheduledDate, maxScheduledDate);
-        } else {
-            events = eventRepository.findAllByOrderByScheduledDate();
-        }
-        model.addAttribute("events", events);
-        model.addAttribute("event", new Event());
-        return "events/events";
     }
 
     @PostMapping("/events/details/{eventId}/comments/new")
