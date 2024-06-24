84778e60261a21b01c854dbddf2092ac3791c4eb
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2024-06-24 14:16:14 +0100

    move events filert to landling page

--- src/main/java/com/makersacademy/acebook/controller/LandingPageController.java
+++ src/main/java/com/makersacademy/acebook/controller/LandingPageController.java
@@ -5,14 +5,18 @@ import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.EventRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @CrossOrigin
 @Controller
@@ -25,12 +29,33 @@ public class LandingPageController {
     private EventRepository eventRepository;
 
     @GetMapping("/")
-    public String listUsersAndEvents(Model model) {
+    public String userEvents(Model model,
+                             @AuthenticationPrincipal Object principal,
+                             @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date minScheduledDate,
+                             @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date maxScheduledDate) {
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else if (principal instanceof OAuth2User) {
+            username = ((OAuth2User) principal).getAttribute("name");
+        } else {
+            username = "User";
+        }
+
+        model.addAttribute("name", username);
+
+        List<Event> events;
+        if (minScheduledDate != null && maxScheduledDate != null) {
+            events = eventRepository.findByScheduledDateBetween(minScheduledDate, maxScheduledDate);
+        } else {
+            events = eventRepository.findAllByOrderByScheduledDate();
+        }
+
         List<User> users = userRepository.findAll();
-        List<Event> events = StreamSupport.stream(eventRepository.findAll().spliterator(), false)
-                .collect(Collectors.toList());
         model.addAttribute("users", users);
         model.addAttribute("events", events);
+        model.addAttribute("event", new Event());
+
         return "landingpage";
     }
 }
