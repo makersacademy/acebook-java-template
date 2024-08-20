1476ab1f384248c49c0ff79c4f36cc1958027613
Edward Andress <eddie_andress@hotmail.com>
2024-08-20 16:46:17 +0100

    complete Auth0 with profile creation

--- src/main/java/com/makersacademy/acebook/config/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/config/SecurityConfiguration.java
@@ -1,42 +1,53 @@
 package com.makersacademy.acebook.config;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+    @Value("${okta.oauth2.client-id}")
+    private String clientId;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/public/**", "/login", "/oauth2/**", "/logout").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/profiles").permitAll()
+                        .requestMatchers("/", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/posts", true)
-                )
+                .oauth2Login(withDefaults())
                 .logout(logout -> logout
-                        .logoutUrl("/logout") // The URL to trigger logout
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll()
-                );
-
+                        .addLogoutHandler(logoutHandler()));
         return http.build();
     }
 
+    private LogoutHandler logoutHandler() {
+        return (request, response, authentication) -> {
+            try {
+                System.out.println("ISSUER");
+                System.out.println(issuer);
+                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
 }
--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -7,30 +7,20 @@ import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
+@RestController
 public class UsersController {
-
     @Autowired
     UserRepository userRepository;
     @Autowired
     AuthoritiesRepository authoritiesRepository;
 
-    @GetMapping("/users/new")
-    public String signup(Model model) {
-        model.addAttribute("user", new User());
-        return "users/new";
-    }
-
-    @PostMapping("/users")
-    public RedirectView signup(@ModelAttribute User user) {
+    @PostMapping("/profiles")
+    public User afterRegistration(@RequestBody User user) {
+        System.out.println(user);
         userRepository.save(user);
-        Authority authority = new Authority(user.getUsername(), "ROLE_USER");
-        authoritiesRepository.save(authority);
-        return new RedirectView("/login");
+        return user;
     }
 }
