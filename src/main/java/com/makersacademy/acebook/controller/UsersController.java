15d1b1f92fbd580ea9f11c3401a21532665fd983
Edward Andress <eddie_andress@hotmail.com>
2024-10-02 17:18:03 +0100

    rm need for localtunnel

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -3,16 +3,28 @@ package com.makersacademy.acebook.controller;
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class UsersController {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping("/users")
-    public User afterRegistration(@RequestBody User user) {
-        userRepository.save(user);
-        return user;
+    @GetMapping("/users/after-login")
+    public RedirectView afterLogin() {
+        DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        String username = (String) principal.getAttributes().get("email");
+        userRepository
+                .findUserByUsername(username)
+                .orElseGet(() -> userRepository.save(new User(username)));
+
+        return new RedirectView("/posts");
     }
 }
--- src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
+++ src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
@@ -1,18 +1,14 @@
+package com.makersacademy.acebook.feature;
+
 import com.github.javafaker.Faker;
-import com.makersacademy.acebook.Application;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest(classes = Application.class)
 public class SignUpTest {
 
     WebDriver driver;
